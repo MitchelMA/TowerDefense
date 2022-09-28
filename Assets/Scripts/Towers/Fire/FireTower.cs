8e8f41c73f584906654a21c3ece85bfec50668e2
MitchelMA <32548@ma-web.nl>
2022-09-28 16:34:43 +0200

    correcte layering

--- Assets/Scripts/Towers/Fire/FireTower.cs
+++ Assets/Scripts/Towers/Fire/FireTower.cs
@@ -8,11 +8,6 @@ namespace Towers.Fire
     // Required implementations only
     public partial class FireTower : BaseTower
     {
-        protected override void AfterGainingXp(int levelsGained, int xpGained)
-        {
-            Debug.Log($"Tower of type {Type} is done Gaining Xp: went from level {CurrentXpStats.currentLvl - levelsGained} to level {CurrentXpStats.currentLvl} after gaining {xpGained} amount of xp\nLvlPoints: {CurrentXpStats.lvlPoints}");
-        }
-
         protected override bool CreateEffect(out BaseEffect effect)
         {
             effect = new FireEffect(_currentEffectStats, Type);
