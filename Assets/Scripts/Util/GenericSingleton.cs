0f66f3f039b2d3cd4068f94a539e5dfbfc14b67d
MitchelMA <32548@ma-web.nl>
2022-11-16 13:01:52 +0100

    stackoverflow fix

--- Assets/Scripts/Util/GenericSingleton.cs
+++ Assets/Scripts/Util/GenericSingleton.cs
@@ -13,8 +13,8 @@ namespace Util
             {
                 if (_instance != null) return _instance;
 
-                _instance = FindObjectOfType<T>();
-                if (Instance != null) return _instance;
+                _instance = GameObject.FindObjectOfType<T>();
+                if (_instance != null) return _instance;
 
                 GameObject container = new GameObject(typeof(T).Name);
                 _instance = container.AddComponent<T>();
